66d203ca6576723befe3ea85abb0d87fc7d6152d
Versatiledd <mddudi94@gmail.com>
2020-01-15 19:21:41 +0100

    adding hover effect

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,7 +1,29 @@
 import React, { useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import gsap from "gsap";
+import AI from "../images/AI.jpg";
+import Deploy from "../images/deploy.jpg";
+import DataScience from "../images/datascience.jpg";
+import Security from "../images/security.jpg";
 
+const ItSection = [
+  {
+    name: "AI",
+    image: AI
+  },
+  {
+    name: "Deploy",
+    image: Deploy
+  },
+  {
+    name: "DataScience",
+    image: DataScience
+  },
+  {
+    name: "Security",
+    image: Security
+  }
+];
 const Menu = ({ state }) => {
   let menu = useRef(null);
   let secondMenu = useRef(null);
@@ -65,48 +87,73 @@ const Menu = ({ state }) => {
       ease: "Power1.easeOut"
     });
   };
+
+  const handleIT = it => {
+    gsap.to(cityBg, {
+      duration: 0,
+      background: `url(${it}) center center`
+    });
+    gsap.to(cityBg, {
+      duration: 0.6,
+      opacity: 1,
+      ease: "Power1.easeOut"
+    });
+    gsap.from(cityBg, {
+      duration: 0.6,
+      transformOrigin: "right top"
+    });
+  };
+  const removeItSection = () => {
+    gsap.to(cityBg, {
+      duration: 0.6,
+      opacity: 0
+    });
+  };
   return (
     <div className="menu-hamburger" ref={el => (menu = el)}>
       <div className="menu-background" ref={el => (menuBg = el)}></div>
       <div className="menu-layer" ref={el => (secondMenu = el)}>
-        <div className="city-bg"></div>
-        <div className="container">
-          <div className="wrapper">
-            <div className="menu-links">
-              <nav>
-                <ul>
-                  <li>
-                    <Link to="/solutions" ref={el => (number1 = el)}>
-                      Solutions
-                    </Link>
-                  </li>
-                  <li>
-                    <Link to="/shop" ref={el => (number2 = el)}>
-                      Shop
-                    </Link>
-                  </li>
-                  <li>
-                    <Link to="/contact" ref={el => (number3 = el)}>
-                      Contact
-                    </Link>
-                  </li>
-                </ul>
-              </nav>
-              <div className="message" ref={el => (message = el)}>
-                <h4>Our dreams</h4>
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Facere, eum rerum. Blanditiis laborum ipsum facilis?
-                  Aspernatur consequatur accusamus debitis dignissimos magnam
-                  voluptate tempore libero quisquam!
-                </p>
-              </div>
-              <div className="location">
-                Kraje : <span>Polska</span>
-                <span>Tokio</span>
-                <span>Iran</span>
-                <span>Portugalia</span>
-              </div>
+        <div className="city-bg" ref={el => (cityBg = el)}></div>
+        <div className="wrapper">
+          <div className="menu-links">
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/solutions" ref={el => (number1 = el)}>
+                    Solutions
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/shop" ref={el => (number2 = el)}>
+                    Shop
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/contact" ref={el => (number3 = el)}>
+                    Contact
+                  </Link>
+                </li>
+              </ul>
+            </nav>
+            <div className="message" ref={el => (message = el)}>
+              <h4>Our dreams</h4>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere,
+                eum rerum. Blanditiis laborum ipsum facilis? Aspernatur
+                consequatur accusamus debitis dignissimos magnam voluptate
+                tempore libero quisquam!
+              </p>
+            </div>
+            <div className="location">
+              {ItSection.map(el => (
+                <span
+                  key={el.name}
+                  onMouseEnter={() => handleIT(el.image)}
+                  onMouseOut={removeItSection}
+                >
+                  {el.name}
+                </span>
+              ))}
             </div>
           </div>
         </div>
