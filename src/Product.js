cc22c3f70a0731da74e9fe1f9619b1e9b47df92c
Versatiledd <mddudi94@gmail.com>
2020-01-18 15:41:48 +0100

    adding button from styled component

--- src/Product.js
+++ src/Product.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import Button from "../src/components/Button";
 import { Link } from "react-router-dom";
 import { ProductConsumer } from "./context/context";
 import "../src/styles/featured-products.css";
@@ -10,13 +11,18 @@ export default function Product({ product }) {
       {value => {
         const { addToCart, setSingleProduct } = value;
         return (
-          <div className="single-product">
-            <div className="card">
-              <div className="img-container">
-                <img className="img-single" src={product.image} alt="" />
+          <>
+            <div className="single-product">
+              <div className="card">
+                <div className="img-container">
+                  <img className="img-single" src={product.image} alt="" />
+                </div>
               </div>
+              <Link to={`/product/${product.id}`}>
+                <Button></Button>
+              </Link>
             </div>
-          </div>
+          </>
         );
       }}
     </ProductConsumer>
---
+++ src/components/Button.js
@@ -0,0 +1,44 @@
+import React from "react";
+import styled from "styled-components";
+
+export default function Button() {
+  return <SingleButton>Kup produkt</SingleButton>;
+}
+
+const SingleButton = styled.button`
+  font-family: "sans-serif";
+  letter-spacing: 1px;
+  cursor: pointer;
+  position: relative;
+  background-color: white;
+  color: black;
+  padding: 10px 20px;
+  font-size: 14px;
+  text-transform: Uppercase;
+  margin: 20px 0;
+  text-decoration: none;
+  width: 160px;
+  height: 40px;
+  outline: none;
+  border: 1px solid gray;
+  overflow: hidden;
+  transition: 1s all ease-in-out;
+  z-index: 0;
+
+  &::before {
+    position: absolute;
+    content: "";
+    top: 50%;
+    left: 50%;
+    width: 0;
+    height: 100%;
+    transform: translate(-50%, -50%) rotate(45deg);
+    background: #e8c064;
+    z-index: -1;
+    transition: 0.5s all ease;
+  }
+  &:hover::before {
+    height: 370%;
+    width: 100%;
+  }
+`;
