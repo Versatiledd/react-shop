6444f848f084699be2f9763182d05cba74cdc4e4
Versatiledd <mddudi94@gmail.com>
2020-02-04 01:24:31 +0100

    styling side-cart

--- src/components/SideCart.js
+++ src/components/SideCart.js
@@ -7,27 +7,32 @@ export default function SideCart({ value }) {
   useEffect(() => {
     if (value.cartOpen === true) {
       gsap.to(var1, {
-        duration: 0.4,
-        height: "50%",
+        opacity: 1,
+        duration: 0.18,
         width: 300,
         ease: "Power1.easeOut"
       });
     } else if (value.cartOpen === false) {
       gsap.to(var1, {
+        duration: 0.18,
         right: 0,
-        width: 0
+        width: 0,
+        opacity: 0
       });
     }
   });
 
   return (
     <div className="overlay" ref={el => (var1 = el)}>
-      <h4>Twój koszyk</h4>
+      <h4 className="overlay-title">Twój koszyk</h4>
       {value.cart.map(item => (
-        <div key={item.id}>
-          <img src={item.image} alt="" />
+        <div key={item.id} className="shopCart">
+          <img src={item.image} alt="" className="shopImage" />
+          <h3 className="shop-title">{item.title}</h3>
+          <span className="shop-price"> Cena : {item.price} zł</span>
         </div>
       ))}
+      <button>Przejdź do karty</button>
     </div>
   );
 }
